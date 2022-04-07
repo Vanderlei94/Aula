6ff4a6c1fef00125b5cc65b59b5492fdcb7e13a2
Vanderlei Oliveira <vanderleicas@gmail.com>
2022-04-07 20:25:53 -0300

    Versão 0.1

---
+++ Aula/Program.cs
@@ -0,0 +1,28 @@
+﻿using Aula.Entities;
+using System;
+using System.Globalization;
+using Aula.Entities.Enums;
+
+namespace Aula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+        };
+        Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
