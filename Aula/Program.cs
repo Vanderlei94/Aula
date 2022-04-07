b8bff9633b6d12adac1da131242e108a80235f72
Vanderlei Oliveira <vanderleicas@gmail.com>
2022-04-07 20:52:09 -0300

    Aula de Enums e composição

--- Aula/Program.cs
+++ Aula/Program.cs
@@ -9,20 +9,7 @@ namespace Aula
     {
         static void Main(string[] args)
         {
-            Order order = new Order
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-        };
-        Console.WriteLine(order);
-
-            string txt = OrderStatus.PendingPayment.ToString();
-
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-
-            Console.WriteLine(os);
-            Console.WriteLine(txt);
+            
         }
     }
 }
